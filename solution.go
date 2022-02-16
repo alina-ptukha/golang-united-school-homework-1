6bf3225fba9c0af4cabbb0fc8ad18058c179cdc7
alina-ptukha <ptukha.alina@gmail.com>
2022-02-16 17:09:46 +0300

    I need some golang

--- solution.go
+++ solution.go
@@ -1,13 +1,13 @@
 package solution
 
 import (
-	
+
     "github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
     hello := "Hello"
-    world := "🗺️"
+    world := ":world_map!:"
     rendered := emoji.Sprint(hello, world)
     rendered = rendered + " !"
     return rendered
