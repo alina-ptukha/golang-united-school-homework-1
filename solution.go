f389c59405304ef8fa410daec812d554d5368ffd
alina-ptukha <ptukha.alina@gmail.com>
2022-02-16 16:12:09 +0300

    My best solution. I love my Milly r

--- solution.go
+++ solution.go
@@ -1,9 +1,10 @@
 package solution 
 
 import (
+	"fmt"
 	"github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
-	return "emoji.Sprint("Hello :world_map!:")"
+	return "emoji.Sprint ("Hello ":world_map!:")"
 }
